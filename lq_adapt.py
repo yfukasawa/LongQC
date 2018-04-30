3a2a1aec602615a1fc8d13113b62fbc0f8d9fca2
fukasay <yoshinori.fukasawa@kaust.edu.sa>
2018-04-30 17:09:53 +0300

    small changes

--- lq_adapt.py
+++ lq_adapt.py
@@ -2,21 +2,23 @@ import os, logging
 import bindings.python.edlib
 
 def _cut(reads, adp, th, r, lengths=[]):
-    iden_max = -1
+    iden_max  = -1
+    match_num = 0
 
     for read in reads:
         lengths.append( len(read[1]) )
         #result5 = bindings.python.edlib.align(, read[1][:150], mode="HW", task='path')
         result = bindings.python.edlib.align(adp, read[1][:r], mode="HW", task='path')
         if result['identity'] >th:
-            #print(read[0], result['identity'], result['cigar'])
+            print(read[0], result['identity'], result['cigar'])
+            print(result)
+            match_num += 1        
             if result['identity'] > iden_max:
                 iden_max = result['identity']
 
-    return iden_max
+    return (iden_max, match_num)
 
-
-def cut_adapter(reads, lengths, logger, adp_t, adp_b = None, th=0.75, length=150):
+def cut_adapter(reads, lengths, adp_t, adp_b = None, logger=None, th=0.75, length=150):
     """
     > sequel_adapter
     ATCTCTCTCAACAACAACAACGGAGGAGGAGGAAAAGAGAGAGAT
@@ -45,7 +47,19 @@ def cut_adapter(reads, lengths, logger, adp_t, adp_b = None, th=0.75, length=150
         return None
     
     if adp_t:
-        iden_max5 = _cut(reads, adp_t, th, length, lengths)
+        (iden_max5, mnum) = _cut(reads, adp_t, th, length, lengths)
+        logger.info("max identity:%f The number of matched read: %d" % (iden_max5, mnum))
 
     if adp_b:
-        iden_max3 = _cut(reads, adp_t, th, length)
+        (iden_max3, mnum) = _cut(reads, adp_t, th, length)
+        logger.info("max identity:%f The number of matched read: %d" % (iden_max3, mnum))
+
+    """
+    print('traverse was finished. %d reads were processed.' % tot)
+    print('Reads having adapter-like seq in both ends: %d / %d' % (trimboth, tot))
+    print('Reads having adapter-like seq in 5\' ends: %d / %d' % (trim5 - trimboth, tot))
+    print('Reads having adapter-like seq in 3\' ends: %d / %d' % (trim3 - trimboth, tot))
+    print('Max identity in the reads: %.3f' % iden_max5)
+    print('Max identity in the reads: %.3f' % iden_max3)
+    print(max3_seq)
+    """
