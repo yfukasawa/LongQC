ae3a9b36ff1590639cb1f309c1f7b1cf819524f3
fukasay <yoshinori.fukasawa@kaust.edu.sa>
2018-05-08 10:59:35 +0300

    Fixed some bugs about return values for 2 adapter cases.

--- lq_adapt.py
+++ lq_adapt.py
@@ -1,24 +1,59 @@
 import os, logging
 import bindings.python.edlib
 
-def _cut(reads, adp, th, r, lengths=[]):
+def _cutr(reads, adp, th, r, *, len_list=[]):
     iden_max  = -1
     match_num = 0
-
+    cut_pos   = []
+    if len(reads[0]) > 2:
+        has_qual = True
+    else:
+        has_qual = False
     for read in reads:
-        lengths.append( len(read[1]) )
-        #result5 = bindings.python.edlib.align(, read[1][:150], mode="HW", task='path')
-        result = bindings.python.edlib.align(adp, read[1][:r], mode="HW", task='path')
+        if not len_list:
+            pass
+        else:
+            len_list.append( len(read[1]) )
+        result = bindings.python.edlib.align(adp, read[1][-r:], mode="HW", task='path')
         if result['identity'] >th:
-            print(read[0], result['identity'], result['cigar'])
-            print(result)
+            #print(read[0], result['identity'], result['locations'], result['cigar'])
+            start = result['locations'][0][0]
+            cut_pos.append(start)
             match_num += 1        
             if result['identity'] > iden_max:
                 iden_max = result['identity']
+            read[1] = read[1][:start]
+            if has_qual:
+                read[2] = read[2][:start]
+    return (iden_max, match_num, cut_pos)
 
-    return (iden_max, match_num)
+def _cutf(reads, adp, th, r, *, len_list=[]):
+    iden_max  = -1
+    match_num = 0
+    cut_pos   = []
+    if len(reads[0]) > 2:
+        has_qual = True
+    else:
+        has_qual = False
+    for read in reads:
+        if not len_list:
+            pass
+        else:
+            len_list.append( len(read[1]) )
+        result = bindings.python.edlib.align(adp, read[1][:r], mode="HW", task='path')
+        if result['identity'] >th:
+            #print(read[0], result['identity'], result['locations'], result['cigar'])
+            end = result['locations'][0][1]
+            cut_pos.append(end)
+            match_num += 1        
+            if result['identity'] > iden_max:
+                iden_max = result['identity']
+            read[1] = read[1][end+1:]
+            if has_qual:
+                read[2] = read[2][end+1:]
+    return (iden_max, match_num, cut_pos)
 
-def cut_adapter(reads, lengths, adp_t, adp_b = None, logger=None, th=0.75, length=150):
+def cut_adapter(reads, *, len_list=None, adp_t=None, adp_b = None, logger=None, th=0.75, length=150):
     """
     > sequel_adapter
     ATCTCTCTCAACAACAACAACGGAGGAGGAGGAAAAGAGAGAGAT
@@ -34,25 +69,29 @@ def cut_adapter(reads, lengths, adp_t, adp_b = None, logger=None, th=0.75, lengt
     GTTTTCGCATTTATCGTGAAACGCTTTCGCGTTTTTCGTGCGCCGCTTCA
     """
 
-    tot       = 0
-    trim5     = 0
-    trim3     = 0
-    trimboth  = 0
     iden_max5 = 0.0
     iden_max3 = 0.0
-    max3_seq  = ''
 
     if not adp_t and not adp_b:
         logger.WARNING()
         return None
-    
-    if adp_t:
-        (iden_max5, mnum) = _cut(reads, adp_t, th, length, lengths)
-        logger.info("max identity:%f The number of matched read: %d" % (iden_max5, mnum))
 
-    if adp_b:
-        (iden_max3, mnum) = _cut(reads, adp_t, th, length)
-        logger.info("max identity:%f The number of matched read: %d" % (iden_max3, mnum))
+    if adp_t and adp_b:
+        (iden_max5, mnum5, cpos5) = _cutf(reads, adp_t, th, length, len_list=len_list)
+        (iden_max3, mnum3, cpos3) = _cutr(reads, adp_t, th, length)
+        logger.info("Adapter Sequence: %s, max identity:%f and the number of trimmed reads: %d" % (adp_t, iden_max5, mnum5))
+        logger.info("Adapter Sequence: %s, max identity:%f and the number of trimmed reads: %d" % (adp_b, iden_max3, mnum3))
+        return ((iden_max5, mnum5, cpos5), (iden_max3, mnum3, cpos3))
+
+    if adp_t and not adp_b:
+        (iden_max5, mnum5, cpos5) = _cutf(reads, adp_t, th, length, len_list=len_list)
+        logger.info("Adapter Sequence: %s, max identity:%f and the number of trimmed reads: %d" % (adp_t, iden_max5, mnum5))
+        return (iden_max5, mnum, cpos)
+
+    if not adp_t and adp_b:
+        (iden_max3, mnum3, cpos3) = _cutr(reads, adp_t, th, length)
+        logger.info("Adapter Sequence: %s, max identity:%f and the number of trimmed reads: %d" % (adp_b, iden_max3, mnum3))
+        return (iden_max3, mnum3, cpos3)
 
     """
     print('traverse was finished. %d reads were processed.' % tot)
