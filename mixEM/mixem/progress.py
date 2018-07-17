0f1526ee467f42ed799d5abbdfa871ca1789438a
fukasay <yoshinori.fukasawa@kaust.edu.sa>
2018-07-17 15:02:45 +0300

    Made changes to accept logger object.

--- mixEM/mixem/progress.py
+++ mixEM/mixem/progress.py
@@ -1,5 +1,5 @@
 # coding=utf-8
-
+import logging
 
 def simple_progress(iteration, weights, distributions, log_likelihood):
     """A simple default progress callback to use with mixem.em"""
@@ -9,3 +9,18 @@ def simple_progress(iteration, weights, distributions, log_likelihood):
         log_likelihood=log_likelihood,
         formatted_distributions=" + ".join("{w:.3g}*{d}".format(w=w, d=d) for w, d in zip(weights, distributions))
     ))
+
+def logged_simple_progress(iteration, weights, distributions, log_likelihood, logger=None):
+
+    if not logger:
+        print("iteration {iteration:4d} (log-likelihood={log_likelihood:.5e}): p(x|Φ) = {formatted_distributions}".format(
+            iteration=iteration,
+            log_likelihood=log_likelihood,
+            formatted_distributions=" + ".join("{w:.3g}*{d}".format(w=w, d=d) for w, d in zip(weights, distributions))
+        ))
+    else:
+        logger.info("iteration {iteration:4d} (log-likelihood={log_likelihood:.5e}): p(x|Φ) = {formatted_distributions}".format(
+            iteration=iteration,
+            log_likelihood=log_likelihood,
+            formatted_distributions=" + ".join("{w:.3g}*{d}".format(w=w, d=d) for w, d in zip(weights, distributions))
+        ))
